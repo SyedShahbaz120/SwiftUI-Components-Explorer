de66a1de5a85a050c0b681dde97b5ca5b457072a
SyedShahbaz120 <syedshahbaz120@gmail.com>
2024-07-19 12:49:15 -0400

    Initial Commit

---
+++ SwiftUI Components Explorer/SwiftUI_Components_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Components_ExplorerApp.swift
+//  SwiftUI Components Explorer
+//
+//  Created by Syed Shahbaz on 2024-07-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Components_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
